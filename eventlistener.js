7e57ee6f504920608a2f846c65d32a218ff433ca
Vincent Yan <67217414+vinceyyy@users.noreply.github.com>
2021-08-03 20:24:02 -0700

    Add South America

--- eventlistener.js
+++ eventlistener.js
@@ -1,7 +1,12 @@
 import wixLocation from "wix-location";
 
 $w.onReady(function () {
-    const regionList = ["southeast-asia", "north-america", "china"];
+    const regionList = [
+        "southeast-asia",
+        "north-america",
+        "china",
+        "south-america",
+    ];
     $w("#html1").onMessage((event) => {
         regionList.forEach((region) => {
             if (event.data === region) {
